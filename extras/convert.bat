a10c6328e831f071ab40e184ce0de6b6d71618ec
cbm80amiga <pawelhernik123@gmail.com>
2021-04-19 11:52:29 +0200

    bin2hex

---
+++ extras/convert.bat
@@ -0,0 +1,23 @@
+bin2hex --i "Sierra 2.jpg" --o sierra2.h
+bin2hex --i Sierra.jpg --o sierra.h
+bin2hex --i "El Capitan 2.jpg" --o elcap.h
+bin2hex --i "Mojave Day.jpg" --o mojave.h
+bin2hex --i "High Sierra.jpg" --o highsierra.h
+bin2hex --i "Flower 7.jpg" --o flower7.h
+bin2hex --i "Flower 5.jpg" --o flower5.h
+bin2hex --i "Flower 10.jpg" --o flower10.h
+bin2hex --i "Desert 5.jpg" --o desert5.h
+bin2hex --i "Desert 3.jpg" --o desert3.h
+bin2hex --i "Desert 2.jpg" --o desert2.h
+bin2hex --i "Desert 1.jpg" --o desert1.h
+bin2hex --i "Reflection 1.jpg" --o refl.h
+bin2hex --i "Ink Cloud.jpg" --o inkcloud.h
+bin2hex --i "tropical.jpg" --o tropical.h
+bin2hex --i "grnpalm.jpg" --o grnpalm.h
+bin2hex --i "glassap.jpg" --o glassap.h
+bin2hex --i "landscap.jpg" --o landscap.h
+bin2hex --i "kitten.jpg" --o kitten.h
+bin2hex --i "phang.jpg" --o phang.h
+bin2hex --i "Earth70.jpg" --o earth70.h
+bin2hex --i "Lake30.jpg" --o lake30.h
+bin2hex --i "Yosemite50.jpg" --o yosemite50.h
